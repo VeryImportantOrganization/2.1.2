b6d3c7a7444d32381b7af79390abe5a460ad3745
phareebo <faribo.gafur@gmail.com>
2023-06-17 17:58:26 +0300

    compared by link

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,7 +18,6 @@ public class App {
         System.out.println(tom.getCatVoice());
 
         System.out.println("HelloWorld's beans equal: " + (bean == bean2));
-
         System.out.println("Cat's beans equal: " + (kitty == tom));
     }
 }
